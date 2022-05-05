017c7e75559304833588c9d4a67f5ce72e84e9d8
AndreasA <eaaaaj@students.eaaa.dk>
2022-05-05 19:13:13 +0200

    SS
    ss

--- bachelortest/src/App.tsx
+++ bachelortest/src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './index.css';
+import { Button } from './stories/Button'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-9xl'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className='h-full'>
+      <p>Hej</p>
+      <Button
+  backgroundColor="#b16565"
+  color="#0a0dff"
+  fontfami="font-serif"
+  label="Button"
+  onClick={() => {}}
+  sizingheight="h-3/4"
+  sizingwidth="w-3/4"
+/>
+</div>
+     
+   
+   
+  )
 }
+
+    
+  
 
 export default App;
